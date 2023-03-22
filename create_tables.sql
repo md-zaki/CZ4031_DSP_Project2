49f7ac289e92eec4c9a795951ada2591bcad4cb0
Md Zaki <mdzaki333@gmail.com>
2023-03-22 22:50:55 +0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -66,9 +66,13 @@ CREATE TABLE public.partsupp
 	ps_comment character varying(199) COLLATE pg_catalog."default" NOT NULL,
 	CONSTRAINT partsupp_pkey PRIMARY KEY (ps_partkey, ps_suppkey),
 	CONSTRAINT fk_ps_suppkey_partkey FOREIGN KEY (ps_partkey)
-		REFERENCES public.supplier (s_suppkey) MATCH SIMPLE
+		REFERENCES public.part (p_partkey) MATCH SIMPLE
 		ON UPDATE NO ACTION
-		ON DELETE NO ACTION
+		ON DELETE NO ACTION,
+    CONSTRAINT fk_ps_suppkey FOREIGN KEY (ps_suppkey)
+        REFERENCES public.supplier (s_suppkey) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 )
 WITH (
 	OIDS = FALSE
@@ -166,6 +170,7 @@ CREATE TABLE public.lineitem
 	l_returnflag character(1) COLLATE pg_catalog."default" NOT NULL,
 	l_linestatus character(1) COLLATE pg_catalog."default" NOT NULL,
 	l_shipdate date NOT NULL,
+    l_commitdate date NOT NULL,
 	l_receiptdate date NOT NULL,
 	l_shipinstruct character(25) COLLATE pg_catalog."default" NOT NULL,
 	l_shipmode character(10) COLLATE pg_catalog."default" NOT NULL,
