0bf8f560d39d97a5b07a3c325f6052b8d70194b5
Md Zaki <mdzaki333@gmail.com>
2023-03-28 22:12:17 +0800

    trying out streamlit with graphviz

--- interface.py
+++ interface.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import graphviz
 
+graph = '''
+    digraph {{
+        {e1} [color ="red"]
+        {e1} -> {e2}
+    }}
+'''.format(e1 = 'element1', e2 = 'element2')
+
+st.graphviz_chart(graph)
