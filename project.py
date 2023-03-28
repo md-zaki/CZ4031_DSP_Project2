ae6d6ff6c2fe1add2e8270d5e0894e56a932624b
paingkl <paingissleeping@gmail.com>
2023-03-29 05:39:00 +0800

    Add tkinter app prototype

--- project.py
+++ project.py
@@ -1,1 +1,64 @@
+from collections import deque
+import psycopg2
 
+from interface import App
+from explain import get_exp
+
+
+def test_func(cursor, app):
+    extract_qp = "EXPLAIN (ANALYZE false, SETTINGS true, FORMAT JSON) "
+    query = app.get_query_input()
+    try:
+        cursor.execute(extract_qp + query)
+    except Exception as e:
+        app.display_error_message(str(e))
+
+    # get query plan in JSON format
+    qep = cursor.fetchall()[0][0][0].get("Plan")
+
+    # make lists of nodes and its sub plans
+    node_list = []
+    q = deque([qep])
+    while q:
+        for i in range(len(q)):
+            node = q.popleft()
+            node_list.append(node)
+            if "Plans" in node:
+                for child in node['Plans']:
+                    q.append(child)
+    # Reverse the list
+    node_list.reverse()
+    
+    # Print Query Execution Plan Tree from Postgres
+    extract_qp = "EXPLAIN (COSTS FALSE, TIMING FALSE) "
+    try:
+        cursor.execute(extract_qp + query)
+    except Exception as e:
+        app.display_error_message(str(e))
+    qep_list1 = cursor.fetchall()
+    qep_display_text = "\n".join([i[0] for i in qep_list1])
+    app.display_query_plan(qep_display_text)
+
+    # Print Explanation
+    exp_list = []
+    count = 1
+    for i in node_list:
+        exp_list.append(str(count) + ". " + get_exp(i))
+        count = count + 1
+    exp_display_text = "\n\n".join([i for i in exp_list])
+    app.display_explanation(exp_display_text)
+
+
+if __name__ == "__main__":
+    # connect to postgres
+    conn = psycopg2.connect(database="TPC-H",
+                            host="localhost",
+                            user="postgres",
+                            password="dspproject123",
+                            port="5432")
+    cursor = conn.cursor()
+
+    app = App()
+    app.q_submit_button.configure(command=lambda: test_func(cursor, app))  # Bind function to submit button
+
+    app.mainloop()  # This must be the very last line
