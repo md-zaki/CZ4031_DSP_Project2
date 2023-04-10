a99843fc966bcaa416ec3d106e5dad3f576f34f6
waterxiv <waterxiv@gmail.com>
2023-04-11 00:18:49 +0800

    small qol
    default input

--- project_streamlit.py
+++ project_streamlit.py
@@ -1,13 +1,23 @@
 import subprocess
 
-print("Enter the name of the database that you want to connect:")
+dbDefault = 'postgres'
+hostDefault = 'localhost'
+userDefault = 'postgres'
+
+print("Enter the name of the database that you want to connect: (Leave empty for default = postgres)")
 db = input()
+if db == '':
+    db = dbDefault
 
-print("Enter the database server address e.g., localhost or an IP address:")
+print("Enter the database server address e.g., localhost or an IP address: (Leave empty for default = localhost)" )
 host = input()
+if host == '':
+    host = hostDefault
 
-print("Enter the username used to authenticate:")
+print("Enter the username used to authenticate: (Leave empty for default = postgres)")
 user = input()
+if user == '':
+    user = userDefault
 
 print("Enter the password used to authenticate:")
 pwd = input()
