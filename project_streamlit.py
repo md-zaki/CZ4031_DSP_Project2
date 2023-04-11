864aae51abd07d0ba7b547cc9bbb4f02eef0f2a1
Paing Khantt Lin <paingissleeping@gmail.com>
2023-04-11 17:38:41 +0800

    Add input validation for postgresql authentication (#5)

--- project_streamlit.py
+++ project_streamlit.py
@@ -1,24 +1,45 @@
 import subprocess
+from psycopg2 import connect, OperationalError
 
 dbDefault = 'postgres'
 hostDefault = 'localhost'
 userDefault = 'postgres'
 
-print("Enter the name of the database that you want to connect: (Leave empty for default = postgres)")
-db = input()
-if db == '':
-    db = dbDefault
+# Use a test connection to validate input for postgresql authentication
+test_conn = None
+while not test_conn:
+    print("Enter the name of the database that you want to connect: (Leave empty for default = postgres)")
+    db = input()
+    if db == '':
+        db = dbDefault
 
-print("Enter the database server address e.g., localhost or an IP address: (Leave empty for default = localhost)" )
-host = input()
-if host == '':
-    host = hostDefault
+    print("Enter the database server address e.g., localhost or an IP address: (Leave empty for default = localhost)" )
+    host = input()
+    if host == '':
+        host = hostDefault
 
-print("Enter the username used to authenticate: (Leave empty for default = postgres)")
-user = input()
-if user == '':
-    user = userDefault
+    print("Enter the username used to authenticate: (Leave empty for default = postgres)")
+    user = input()
+    if user == '':
+        user = userDefault
 
-print("Enter the password used to authenticate:")
-pwd = input()
+    print("Enter the password used to authenticate:")
+    pwd = input()
+
+    try:
+        test_conn = connect(database=db, 
+                            host=host, 
+                            user=user, 
+                            password=pwd, 
+                            port="5432")
+    except OperationalError as err:
+        print(err)
+        print("Please provide correct database connection parameters")
+        test_conn = None
+
+# Authentication successful, can close test connection
+test_conn.close()
+test_conn = None
+
+# Run app
 subprocess.run(["streamlit","run","interface_streamlit.py", "--", "--db", db, '--host', host, '--user', user, '--pwd', pwd])
