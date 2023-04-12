ae222aa3dbb8412cd6b058df935d4551bdbd3956
Md Zaki <mdzaki333@gmail.com>
2023-04-12 13:22:47 +0800

    Update explain.py

--- explain.py
+++ explain.py
@@ -297,17 +297,17 @@ def qep_diff_exp(missing1, missing2):
             exp_str1 (str): string of explanation of changes of operations between the 2 QEPs
     """
     if(missing2):
-        exp_str1 = 'the following operations from the original query plan: '
+        exp_str1 = 'The following operations from the original query plan: '
         for i in missing2:
             exp_str1 = exp_str1 + str(i) + ', '
         exp_str1 = exp_str1 + 'were replaced by '
         if(missing1):
-            exp_str1 = exp_str1 + 'the following operations: '
+            exp_str1 = exp_str1 + 'The following operations: '
             for i in missing1:
                 exp_str1 = exp_str1 + str(i) + ', '
             exp_str1 = exp_str1 + ' in the evolved query plan'
     elif(missing1):
-        exp_str1 = 'the following operations were added to the evolved query plan: '
+        exp_str1 = 'The following operations were added to the evolved query plan: '
         for i in missing1:
             exp_str1 = exp_str1 + str(i) + ', '
 
